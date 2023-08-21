5e98453a058d21b5e5a4b2e0f27bdc3dc19d16be
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-08-21 02:17:41 +0200

    add service: system design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,6 +136,10 @@ export default function Home() {
 											Consultancy
 										</TabsTrigger>
 
+										<TabsTrigger value="system-design">
+											System Design
+										</TabsTrigger>
+
 										<TabsTrigger value="frontend">
 											Frontend
 										</TabsTrigger>
@@ -155,6 +159,20 @@ export default function Home() {
 										<p>Estimate:</p>
 										<p className="text-primary">
 											R150/hour
+										</p>
+									</div>
+								</TabsContent>
+
+								<TabsContent value="system-design">
+									This service includes the design and
+									planning of a small/medium web application,
+									including database ERM, user flows, and
+									sequency diagrams.
+									<Separator className="my-2" />
+									<div className="flex flex-row justify-between">
+										<p>Estimate:</p>
+										<p className="text-primary">
+											R1,000-R3,000
 										</p>
 									</div>
 								</TabsContent>
